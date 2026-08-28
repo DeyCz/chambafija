1e2c25dc1698f96cf93b476fe5e0bf831cd50fb1
AlejandroCCCodes <deyvidcalzada2@gmail.com>
2026-08-28 15:19:38 -0500

    Corregir puerto dinamico para Render

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.delete('/api/jobs/:id', async (req, res) => {
     }
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`🚀 Servidor backend corriendo en el puerto ${PORT}`);
 });
